8dc1a4f467573751bd3261c8a696e249a9a18194
Arthur Poulet <arthur.poulet@cryptolab.net>
2014-11-15 13:29:13 +0100

    fix issue on id (no * or ?)

--- api_france/searchable.rb
+++ api_france/searchable.rb
@@ -4,8 +4,8 @@ module Searchable
   def api_search(table, params)
     out = self
     out = out.where('name LIKE ?', params['name_like'].sql_parse_like) if params['name_like']
-    out = out.where('id >= ?', params['id_greater'].sql_parse_like) if params['id_greater']
-    out = out.where('id <= ?', params['id_lesser'].sql_parse_like) if params['id_lesser']
+    out = out.where('id >= ?', params['id_greater'].to_i) if params['id_greater']
+    out = out.where('id <= ?', params['id_lesser'].to_i) if params['id_lesser']
     out = out.where('zipcode LIKE (\'%?%\')', params['zipcode'].to_i) if table == City and params['zipcode']
     return out
   end
